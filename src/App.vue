923e87ca65e94d5e78d6e2075e531a70d85edcf9
leoxuVancouver <99696932+leoxuVancouver@users.noreply.github.com>
2023-06-16 00:15:12 -0700

    Add files via upload
    upload files

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <MapLocation/>
+  </div>
+</template>
+
+<script>
+import MapLocation from './mapLocation.vue'
+
+
+
+export default {
+  name: 'App',
+  components:{
+    MapLocation
+  }
+}
+</script>
+
+
